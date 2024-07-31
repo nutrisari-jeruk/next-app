09b24ee9e68206117e0d6ed0fd5c191e49a29ec5
Arifin Nor <arifinnor03@gmail.com>
2024-07-31 15:21:08 +0700

    fix: add response type

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,17 +1,19 @@
 import $http from '@/lib/axios';
 import { AxiosError } from 'axios';
+import type { BaseResponse } from '@/types/api';
+import type { User } from '@/types/user';
 
 export const dynamic = 'force-dynamic'; // defaults to auto
 export async function GET() {
   try {
-    const { data } = await $http.get('/v1/user');
+    const { data } = await $http.get<BaseResponse<User[]>>('/v1/contact_point');
 
     return Response.json(data);
   } catch (error) {
     if (error instanceof AxiosError) {
-      return Response.json({
-        message: error.message,
-      });
+      return Response.json(error.response?.data);
     }
+
+    throw error;
   }
 }
