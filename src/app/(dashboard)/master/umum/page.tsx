74734cb478247dd193865a6b0d988368bcea5a8c
zulfikrialia <zulfikri.admadja@gmail.com>
2024-08-05 14:56:36 +0700

    wip: jurnal umum feature

--- src/app/(dashboard)/master/umum/page.tsx
+++ src/app/(dashboard)/master/umum/page.tsx
@@ -1,26 +1,31 @@
 import { TwButton, TwHeader } from '@/components';
 import { PlusCircleIcon, PrinterIcon } from '@heroicons/react/24/outline';
 import Table from './components/table';
-import type { Action } from '@/types/header';
+import Link from 'next/link';
 
-export default function page() {
-  const actions: Action[] = [
-    {
-      title: 'Add',
-      icon: <PlusCircleIcon className="h-5 w-5" aria-hidden="true" />,
-      href: '/master/umum/create',
-      variant: 'success',
-    },
-    {
-      title: 'Print',
-      icon: <PrinterIcon className="h-5 w-5" aria-hidden="true" />,
-      href: '/master/umum/print',
-      variant: 'secondary',
-    },
-  ];
+export default function Page() {
+
   return (
     <>
-      <TwHeader title="Jurnal Umum" button={actions} />
+      <div className="flex items-center justify-between">
+        <TwHeader title="Jurnal Umum" />
+        <div className="flex gap-2">
+          <Link href="/master/umum/create">
+            <TwButton
+              title="Add"
+              variant="success"
+              icon={<PlusCircleIcon className="h-5 w-5" />}
+            />
+          </Link>
+          <Link href="/master/umum/print">
+            <TwButton
+              title="Print"
+              variant="secondary"
+              icon={<PrinterIcon className="h-5 w-5" />}
+            />
+          </Link>
+        </div>
+      </div>
       <div className="mt-4 flex flex-col gap-4"> 
         <Table />
       </div>
