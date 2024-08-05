afcf138a821a7b4ac430e6d5c97e1859148aac37
blangkonsultan <bagaspbsc@gmail.com>
2024-08-05 14:37:09 +0700

    wip: jurnal penyesuaian feature

--- src/components/tw-input/index.tsx
+++ src/components/tw-input/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Input as HeadlessInput } from '@headlessui/react';
-import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
+import { Input } from '@headlessui/react';
 import clsx from 'clsx';
 
 interface TwInput extends React.InputHTMLAttributes<HTMLInputElement> {
   name?: string;
   label?: string;
   icon?: React.ReactNode;
+  iconPosition?: 'left' | 'right';
   isError?: boolean;
   required?: boolean;
   errorMessage?: string | string[];
@@ -17,9 +17,10 @@ export default function TwInput(props: TwInput) {
   const randomId = Math.random().toString(36).slice(2);
   const {
     name = `input-${randomId}`,
-    label = 'Input',
+    label = '',
     isError = false,
     icon = null,
+    iconPosition = 'left',
     errorMessage = 'Error message',
     ...attr
   } = props;
@@ -33,21 +34,29 @@ export default function TwInput(props: TwInput) {
         {attr.required && <span className="text-red-500">* </span>}
         {label}
       </label>
-      <div className="relative mt-2 rounded-md shadow-sm">
-        {icon}
-        <HeadlessInput
+      <div className={clsx('relative mt-2 rounded-md shadow-sm')}>
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2">
+          {iconPosition === 'left' && icon}
+        </div>
+        <Input
           name={name}
           id={name}
           className={clsx(
             isError &&
               'text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500',
-            attr.disabled && 'cursor-not-allowed bg-gray-100',
-            'block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
+            attr.disabled ||
+              (attr.readOnly && 'cursor-not-allowed bg-gray-100'),
+            icon && (iconPosition === 'left' ? 'pl-10' : 'pr-10'),
+            'block w-full rounded-md border-0 py-1.5 ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
           )}
           aria-invalid={isError}
           aria-describedby={`${name}-error`}
           {...attr}
         />
+
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+          {iconPosition === 'right' && icon}
+        </div>
       </div>
       {isError && (
         <p className="mt-2 text-sm text-red-600" id={`${name}-error`}>
--- src/types/header.ts
+++
@@ -1,7 +0,0 @@
-export interface Action {
-  title: string;
-  icon: JSX.Element;
-  href: string;
-  variant?: string;
-  size?: string;
-}
