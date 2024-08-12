4dddefd26ce59304613963287e7455e12a3c2722
Arifin Nor <arifinnor03@gmail.com>
2024-08-12 23:04:03 +0700

    fix: not redirected when logged in

--- src/app/ui/partials/user-profile/index.tsx
+++ src/app/ui/partials/user-profile/index.tsx
@@ -4,7 +4,7 @@ import type { User } from 'next-auth';
 export default function UserProfile({ user }: { user: User }) {
   return (
     <div className="flex items-center gap-2">
-      <UserCircleIcon className="h-12 w-12" />
+      <UserCircleIcon className="h-8 w-8" />
       <div className="flex flex-col">
         <span className="text-sm font-semibold">{user.name}</span>
         <span className="text-sm">{user.user_role}</span>
