88baa03c437fc46b73344299055c70f0394f946a
ekasepppp1992 <septiyan.rs@gmail.com>
2024-08-26 07:34:22 +0700

    feat jurnal koreksi

--- src/app/(dashboard)/master/jurnal/koreksi/actions/index.tsx
+++ src/app/(dashboard)/master/jurnal/koreksi/actions/index.tsx
@@ -6,13 +6,67 @@ import { redirect } from 'next/navigation';
 import { revalidatePath } from 'next/cache';
 
 import { AxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import type { PaginateList, PostRequest } from '@/types/penyesuaian';
 import { KoreksiSchema } from '../schema';
-import { PostRequest } from '@/types/penyesuaian';
+import type { Params } from '@/types/params';
+import type { BaseResponse } from '@/types/api';
 
-export async function createKoreksi(
-  _prevState: unknown,
-  formData: FormData,
-) {
+const fetchKoreksiList = async ({
+  page,
+  rowsPerPage,
+  searchField,
+  searchValue,
+}: Params): Promise<PaginateList> => {
+  console.log('cek')
+  let list: PaginateList = {
+    data: [],
+    links: [],
+    meta: {
+      current_page: '0',
+      from: '0',
+      last_page: '0',
+      path: '',
+      per_page: '0',
+      to: '0',
+      total: '0',
+    },
+  };
+
+  try {
+    const { data } = await $http.get<BaseResponse<PaginateList>>(
+      '/v1/masters/journals/correction',
+      {
+        params: {
+          page: page,
+          rowsPerPage: rowsPerPage,
+          searchField: searchField,
+          searchValue: searchValue,
+        },
+      },
+    );
+    console.log('tes');
+
+    if (data.success) {
+      list = data?.data!;
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return list;
+    }
+  }
+
+  return list;
+};
+
+//import { PostRequest } from '@/types/penyesuaian';
+
+//export async function createKoreksi(
+ // _prevState: unknown,
+ // formData: FormData,
+//) {
+
+  const createKoreksi = async (_prevState: unknown, formData: FormData) => {
   const validatedFields = KoreksiSchema.safeParse({
     jenis_jurnal: formData.get('jenis_jurnal'),
     debit: formData.get('debit'),
@@ -38,9 +92,10 @@ export async function createKoreksi(
   try {
     const { data: result } = await $http.post(
      // 'http://10.10.12.26:8000/api/v1/masters/journals/adjustment',
-      'http://10.10.12.26:8000/api/v1/masters/journals/correction',
+      '/v1/masters/journals/correction',
       requestKoreksi,
     );
+
   } catch (error) {
     if (error instanceof AxiosError) {
       if (error.response?.data?.message) {
@@ -57,13 +112,19 @@ export async function createKoreksi(
     }
 
     return {
-      message: 'Something Went Wrong!',
+      message: 'Telah Terjadi Kesalahan Silahkan Hubungi Administrator IT!',
       status: 'error',
     };
   }
 
+  const session = cookies();
+  session.set('toastMessage', 'Data Koreksi Berhasil Dibuat');
+  session.set('toastStatus', 'success');
   revalidatePath('/master/jurnal/koreksi');
   redirect(
     `/master/jurnal/koreksi?message=${encodeURIComponent('Koreksi Created Successfully')}&status=success`,
   );
 }
+
+export { createKoreksi, fetchKoreksiList };
+
--- src/app/(dashboard)/master/jurnal/koreksi/page.tsx
+++ src/app/(dashboard)/master/jurnal/koreksi/page.tsx
@@ -1,30 +1,105 @@
-import { TwButton, TwHeader, TwToast } from '@/components';
-import { PlusCircleIcon, PrinterIcon } from '@heroicons/react/24/outline';
-import Table from './components/table';
 import Link from 'next/link';
+import Search from '@/app/ui/search';
+import { TwButton, TwHeader, TwTable, TwToast } from '@/components';
+import { PlusCircleIcon } from '@heroicons/react/24/outline';
+import { Metadata } from 'next';
+import { fetchKoreksiList } from './actions';
+import { Suspense } from 'react';
+import { cookies } from 'next/headers';
+import type { Params } from '@/types/params';
+import type { Column, Row } from '@/types/table';
+import type { List } from '@/types/penyesuaian';
 
-export default function Page({ searchParams }: any) {
+export const metadata: Metadata = {
+  title: 'Jurnal Penyesuaian',
+};
+export default async function Page({ searchParams }: { searchParams: Params }) {
+  const searchValue = searchParams?.searchValue || '';
+  const searchField = searchParams?.searchField || 'jurnal_kode';
+  const currentPage = searchParams?.page || '1';
+  const perPage = searchParams?.rowsPerPage || '10';
+
+  const session = cookies();
+
+  const columns: Column[] = [
+    {
+      label: 'Kode Jurnal',
+      accessor: 'jurnal_kode',
+      sortable: true,
+    },
+    {
+      label: 'Jenis Jurnal',
+      accessor: 'jurnal_jenis',
+      sortable: true,
+    },
+    {
+      label: 'Debit',
+      accessor: 'debit',
+      sortable: false,
+    },
+    {
+      label: 'Kredit',
+      accessor: 'credit',
+      sortable: false,
+    },
+  ];
+
+  const data = await fetchKoreksiList({
+    page: currentPage,
+    rowsPerPage: perPage,
+    searchField: searchField,
+    searchValue: searchValue,
+  });
+  console.log(data)
+
+  const rows = data?.data.map((item: List) => {
+    const deb = item.kode_rekening.find(
+      (rekening) => rekening.debit !== null && rekening.credit === null,
+    )?.debit;
+    const cred = item.kode_rekening.find(
+      (rekening) => rekening.credit !== null && rekening.debit === null,
+    )?.credit;
+
+    return {
+      id: item.id,
+      jurnal_kode: item.jurnal_kode,
+      jurnal_jenis: item.jurnal_jenis,
+      debit: deb,
+      credit: cred,
+    };
+  }) as Row[];
+
   return (
-    <>
-      {searchParams.message && searchParams.status && (
-        <TwToast message={searchParams.message} status="success" />
-      )}
+    <div>
+      {session.get('toastMessage')?.value &&
+        session.get('toastStatus')?.value && (
+          <TwToast
+            message={session.get('toastMessage')?.value!}
+            status={session.get('toastStatus')?.value!}
+          />
+        )}
       <div className="flex items-center justify-between">
         <TwHeader title="Jurnal Koreksi" />
-        <div className="flex gap-2">
-          <Link href="/master/jurnal/koreksi/create">
-            <TwButton
-              title="Add"
-              variant="success"
-              icon={<PlusCircleIcon className="h-5 w-5" />}
-            />
-          </Link>
-        </div>
+
+        <Link href="/master/jurnal/koreksi/create">
+          <TwButton
+            title="Tambah Baru"
+            variant="success"
+            icon={<PlusCircleIcon className="h-5 w-5" />}
+          />
+        </Link>
       </div>
-      <div className="mt-4 flex flex-col gap-4">
-        <Table />
+
+      <div className="mt-4 flex flex-col space-y-2">
+        <Search placeholder="Cari kode rekening" searchField={searchField} />
+
+        <Suspense
+          key={searchValue + currentPage + perPage}
+          fallback={<div>Loading...</div>}
+        >
+          <TwTable {...{ columns, rows }} />
+        </Suspense>
       </div>
-    </>
+    </div>
   );
 }
-
