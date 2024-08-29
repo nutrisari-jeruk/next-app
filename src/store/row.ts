778a7a15fdb1f2ac883603f00ad46853630f6202
Arifin Nor <arifinnor03@gmail.com>
2024-08-29 10:37:50 +0700

    feat: redirect to previous page
    user now redirect to previous page after edit mapping

--- src/store/row.ts
+++ src/store/row.ts
@@ -1,14 +1,24 @@
 import { create } from 'zustand';
+import type { Params } from '@/types/params';
 import type { Row } from '@/types/table';
 
 interface Props {
   rows: Row[];
+  params: Params;
   setRows: (rows: Row[]) => void;
+  setParams: (params: Params) => void;
 }
 
 const useRowStore = create<Props>((set) => ({
   rows: [],
+  params: {
+    page: '1',
+    rowsPerPage: '10',
+    searchField: '',
+    searchValue: '',
+  },
   setRows: (rows: Row[]) => set({ rows: rows }),
+  setParams: (params: Params) => set({ params: params }),
 }));
 
 export default useRowStore;
