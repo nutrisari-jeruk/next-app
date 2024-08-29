892f36819d10f7ef8ac0da4dae05d7c024fd0a2b
zulfikrialia <zulfikri.admadja@gmail.com>
2024-08-29 12:37:05 +0700

    adjustment jurnal penutup

---
+++ src/app/(dashboard)/master/jurnal/penutup/actions/index.tsx
@@ -0,0 +1,115 @@
+'use server';
+
+import $http from '@/lib/axios';
+import { PenutupSchema} from '../schema';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import { AxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import type { List, PostRequest } from '@/types/penutup';
+import type { Params } from '@/types/params';
+import type { BaseResponse } from '@/types/api';
+import { Pagination } from '@/types/pagination';
+
+
+const fetchPenutupList = async ({
+  page,
+  rowsPerPage,
+  searchField,
+  searchValue,
+}: Params): Promise<Pagination<List[]>> => {
+  let list: Pagination<List[]> = {
+    data: [],
+    links: [],
+    meta: {
+      current_page: 0,
+      from: 0,
+      last_page: 0,
+      path: '',
+      per_page: 0,
+      to: 0,
+      total: 0,
+    },
+  };
+
+  try {
+    const { data } = await $http.get<BaseResponse<Pagination<List[]>>>(
+      '/v1/masters/journals/closing',
+      {
+        params: {
+          page: page,
+          rowsPerPage: rowsPerPage,
+          searchField: searchField,
+          searchValue: searchValue,
+        },
+      },
+    );
+
+    if (data.success) {
+      list = data?.data!;
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return list;
+    }
+  }
+
+  return list;
+};
+
+const createPenutup = async (_prevState: unknown, formData: FormData) => {
+  const validatedFields = PenutupSchema.safeParse({
+    jenis_jurnal: formData.get('jenis_jurnal'),
+    debit: formData.get('debit'),
+    credit: formData.get('credit'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      validationErrors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const requestPenutup: PostRequest = {
+    jenis_jurnal: validatedFields.data.jenis_jurnal,
+    kode_rekening_id: {
+      debit: Number(validatedFields.data.debit),
+      credit: Number(validatedFields.data.credit),
+    },
+  };
+
+  try {
+    await $http.post(
+      '/v1/masters/journals/closing',
+      requestPenutup,
+    );
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.data?.message) {
+        return {
+          message: error.response?.data?.message,
+          status: 'error',
+        };
+      }
+
+      return {
+        message: error.message,
+        status: 'error',
+      };
+    }
+
+    return {
+      message: 'Telah Terjadi Kesalahan Silahkan Hubungi Administrator IT!',
+      status: 'error',
+    };
+  }
+
+  const session = cookies();
+  session.set('toastMessage', 'Data Penyesuaian Berhasil Dibuat');
+  session.set('toastStatus', 'success');
+
+  revalidatePath('/master/jurnal/penutup');
+  redirect(`/master/jurnal/penutup`);
+};
+
+export { createPenutup, fetchPenutupList };
--- src/types/penutup.ts
+++ src/types/penutup.ts
@@ -1,15 +1,45 @@
+import type { Url } from 'next/dist/shared/lib/router/router';
+
+export interface PaginateList {
+  data: List[];
+  links: Links[];
+  meta: Meta;
+}
+
 export interface List {
-    id: number;
-    kode: string;
-    jenis: string;
-    rekening: string;
-    debit: string;
-    kredit: string;
-  }
-  
-  export interface request {
-    kode: 'JU';
-    jenis: string;
+  id: number;
+  jurnal_kode: string;
+  jurnal_jenis: string;
+  kode_rekening: KodeRekening[];
+}
+
+export interface KodeRekening {
+  id: number;
+  code: string;
+  debit?: string | null;
+  credit?: string | null;
+}
+
+export interface Links {
+  url: Url;
+  label: string;
+  active: boolean;
+}
+
+export interface Meta {
+  current_page: string;
+  from: string;
+  last_page: string;
+  path: Url;
+  per_page: string;
+  to: string;
+  total: string;
+}
+
+export interface PostRequest {
+  jenis_jurnal: string;
+  kode_rekening_id: {
     debit: number;
-    kredit: number;
-  }
+    credit: number;
+  };
+}
