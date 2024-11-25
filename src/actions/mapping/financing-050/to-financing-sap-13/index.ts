27fa42366d464546f76f9c1963e526a8fba54647
Arifin Nor <arifinnor03@gmail.com>
2024-11-25 13:26:09 +0700

    feat: add financing mapping

---
+++ src/actions/mapping/financing-050/to-financing-sap-13/index.ts
@@ -0,0 +1,110 @@
+'use server';
+
+import $fetch from '@/lib/fetch';
+import { MapSchema } from '@/schemas/expenditure-050/to-expenditure-sap-13';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { setFlash } from '@/lib/flash-toaster';
+import type { List, Payload } from '@/types/mapping';
+import type { Params } from '@/types/params';
+import type { Pagination } from '@/types/pagination';
+
+const fetchList = async ({
+  page = '1',
+  rowsPerPage = '10',
+  searchField = 'account_050',
+  searchValue = '',
+}: Params): Promise<Pagination<List[]>> => {
+  const params = {
+    page: page,
+    rowsPerPage: rowsPerPage,
+    searchField: searchField,
+    searchValue: searchValue,
+  };
+
+  let list: Pagination<List[]> = {
+    data: [],
+    links: [],
+    meta: {
+      current_page: 0,
+      from: 0,
+      last_page: 0,
+      path: '',
+      per_page: 0,
+      to: 0,
+      total: 0,
+    },
+  };
+
+  const urlParams = new URLSearchParams(params).toString();
+  const data = await $fetch<Pagination<List[]>>({
+    method: 'GET',
+    url: `/v1/mappings/financing-050/financing-sap13?${urlParams}`,
+  });
+
+  if (data.success) {
+    list = data?.data!;
+  }
+
+  return list;
+};
+
+const mapOnAccount = async (_prevState: unknown, formData: FormData) => {
+  const id = formData.get('id');
+  const page = formData.get('page');
+
+  const validatedFields = MapSchema.safeParse({
+    kr050_id: Number(formData.get('kr050_id')),
+    sap13_id: Number(formData.get('sap13_id')),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to map on account',
+    };
+  }
+
+  const payload: Payload = {
+    data: [
+      {
+        label: 'kr050_id',
+        value: validatedFields.data.kr050_id,
+      },
+      {
+        label: 'sap13_id',
+        value: validatedFields.data.sap13_id,
+      },
+    ],
+  };
+
+  try {
+    if (!!id) {
+      await $fetch({
+        url: `/v1/mappings/financing-050/financing-sap13/${id}`,
+        method: 'PUT',
+        payload: payload,
+      });
+    } else {
+      await $fetch({
+        url: '/v1/mappings/financing-050/financing-sap13',
+        method: 'POST',
+        payload: payload,
+      });
+    }
+  } catch (error) {
+    return {
+      message: 'Failed to map on account',
+    };
+  }
+
+  setFlash({
+    type: 'success',
+    message: 'Data berhasil disimpan',
+    tag: new Date().toLocaleString(),
+  });
+  revalidatePath(`/mapping/financing-050/to-financing-sap-13?page=${page}`);
+  redirect(`/mapping/financing-050/to-financing-sap-13?page=${page}`);
+};
+
+export { fetchList, mapOnAccount };
---
+++ src/app/(dashboard)/mapping/financing-050/to-financing-sap-13/page.tsx
@@ -0,0 +1,87 @@
+import { TwHeader } from '@/components';
+import RowProvider from '@/providers/row';
+import Search from '@/components/tw-search';
+import DataTable from '@/app/ui/data-table';
+import { Pagination } from '@/app/ui/data-table/partials';
+import { fetchList } from '@/actions/mapping/financing-050/to-financing-sap-13';
+import type { Metadata } from 'next';
+import type { Params } from '@/types/params';
+import type { Column, Row } from '@/types/table';
+
+export const metadata: Metadata = {
+    title: 'Mapping',
+  };
+
+export default async function Page({
+  searchParams: {
+    page = '1',
+    rowsPerPage = '10',
+    searchField = 'account_050',
+    searchValue = '',
+  },
+}: {
+  searchParams: Params;
+}) {
+  const params: Params = {
+    page: page,
+    rowsPerPage: rowsPerPage,
+    searchField: searchField,
+    searchValue: searchValue,
+  };
+
+  const data = await fetchList(params);
+
+  const columns: Column[] = [
+    {
+      label: '#',
+      accessor: '#',
+    },
+    {
+      label: 'Uraian',
+      accessor: 'account_050',
+      width: '50px',
+      sortable: true,
+    },
+    {
+      label: 'Uraian',
+      accessor: 'account_sap13',
+      width: '50px',
+      sortable: true,
+    },
+    {
+      label: 'Edit',
+      accessor: 'kr050_id',
+    },
+  ];
+
+  const rows = data.data.map((row) => {
+    return {
+      id: row.id,
+      kr050_id: row.kr050_id,
+      sap13_id: row.sap13_id,
+      account_050: row.account_050,
+      account_sap13: row.account_sap13,
+    };
+  }) as Row[];
+
+  const meta = data.meta;
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <TwHeader title="Mapping Kode Rekening Pembiayaan SAP 13" />
+      </div>
+
+      <div className="mt-4 flex w-full flex-col space-y-2">
+        <RowProvider {...{ rows, params }}>
+          <Search
+            placeholder="Cari kode rekening"
+            searchField={searchField}
+          />
+          <DataTable {...{ columns, rows }} />
+          <Pagination {...{ meta }} />
+        </RowProvider>
+      </div>
+    </>
+  );
+}
