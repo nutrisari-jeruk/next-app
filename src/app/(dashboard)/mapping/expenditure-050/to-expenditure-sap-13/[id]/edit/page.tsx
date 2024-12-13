e35d193a53e0dbf1038d2f92f70202b667017953
Arifin Nor <arifinnor03@gmail.com>
2024-12-13 09:20:59 +0700

    fix: assets account now shown

--- src/app/(dashboard)/mapping/expenditure-050/to-expenditure-sap-13/[id]/edit/page.tsx
+++ src/app/(dashboard)/mapping/expenditure-050/to-expenditure-sap-13/[id]/edit/page.tsx
@@ -1,11 +1,16 @@
 import Form from '@/app/(dashboard)/mapping/expenditure-050/to-expenditure-sap-13/ui/form/edit';
 import { TwHeader } from '@/components';
+import { fetchSap13 } from '@/hooks/sap13';
+import type { TreeNode } from '@/types/tree-view';
 
-export default function Page({ params }: { params: { id: string } }) {
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const treeData: TreeNode[] = await fetchSap13({ accounts: ['5'] });
+  
   return (
     <>
       <TwHeader title="Buat Mapping" />
-      <Form {...{ params }} />
+      <Form {...{treeData, params }} />
     </>
   );
 }
